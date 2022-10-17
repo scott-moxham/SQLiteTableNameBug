9d6e4c984976a01d782ef06697e37d36f0ca5607
Scott Moxham <scott.moxham@kornferry.com>
2022-10-17 14:27:48 +0100

    Add project files.

---
+++ SQLiteTableNameBug/Program.cs
@@ -0,0 +1,16 @@
+﻿using static Microsoft.EntityFrameworkCore.RelationalEntityTypeExtensions;
+
+namespace SQLiteTableNameBug;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        using var db = new BloggingContext();
+        db.Database.EnsureCreated();
+
+        var tableName = db.Blogs.EntityType.GetSchemaQualifiedTableName();
+
+        Console.WriteLine($"The table name {tableName} is invalid in SQLite");
+    }
+}
